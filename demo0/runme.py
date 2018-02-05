dfe1778a3716cbb04db90464171e9975a776639f
Gregor GreenInfo <gregor@greeninfo.org>
2018-02-05 21:48:41 +0000

    demo0, added timing output

--- demo0/runme.py
+++ demo0/runme.py
@@ -5,12 +5,21 @@ Load the EE API and print metadata abnout one of their image datasets
 """
 
 import ee
+import time
 
 
 # initialize the Earth Engine object, using the authentication credentials.
+t0 = time.time()
 ee.Initialize()
+t1 = time.time()
+print("Initialize: {0:.1f} seconds".format(t1 - t0))
 
 # print the information for an image asset
+t0 = time.time()
 image = ee.Image('srtm90_v4')
-print(image.getInfo())
+output = image.getInfo()
+t1 = time.time()
+print("image.getInfo: {0:.1f} seconds".format(t1 - t0))
 
+print("Image info output:")
+print(output)
